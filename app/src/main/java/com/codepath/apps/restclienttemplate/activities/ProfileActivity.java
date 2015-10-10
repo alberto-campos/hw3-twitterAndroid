c1f2f867c70d12efc2b43e84b17bb95d8bc67fb6
Alberto Campos <xopmac@gmail.com>
2015-10-10 15:43:47 -0700

    adding followers and following count

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/ProfileActivity.java
@@ -5,20 +5,40 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.restclienttemplate.R;
 import com.codepath.apps.restclienttemplate.TwitterApplication;
 import com.codepath.apps.restclienttemplate.TwitterClient;
 import com.codepath.apps.restclienttemplate.fragments.UserTimelineFragment;
+import com.codepath.apps.restclienttemplate.models.User;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.squareup.picasso.Picasso;
+
+import org.apache.http.Header;
+import org.json.JSONObject;
 
 public class ProfileActivity extends ActionBarActivity {
     TwitterClient client;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setTitle("Retrieving...");
         setContentView(R.layout.activity_profile);
         client = TwitterApplication.getTwitterClient();
+        client.getMyProfile(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                user = User.fromJSON(response);
+                // Return the current's user account info
+                getSupportActionBar().setTitle(user.getName());
+                populateProfileHeader(user);
+            }
+        });
+
         // Get the screen name from activity that Launches this
         String screenName = getIntent().getStringExtra("screen_name");
         if (savedInstanceState == null) {
@@ -29,6 +49,21 @@ public class ProfileActivity extends ActionBarActivity {
             ft.replace(R.id.flContainer, fragmentUserTimeline);
             ft.commit();
         }
+    }
+
+    private void populateProfileHeader(User user) {
+        TextView tvName = (TextView) findViewById(R.id.tvName);
+        TextView tvTagline = (TextView) findViewById(R.id.tvTagline);
+        TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
+        TextView tvFollowing = (TextView) findViewById(R.id.tvFollowing);
+        ImageView ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
+
+        tvName.setText(user.getName());
+        tvTagline.setText(user.getTagline());
+        tvFollowers.setText(user.getFollowersCount() + " followers");
+        tvFollowing.setText(user.getFriendsCount() + " following");
+        Picasso.with(this).load(user.getProfileImageUrl()).into(ivProfileImage);
+
     }
 
 }
